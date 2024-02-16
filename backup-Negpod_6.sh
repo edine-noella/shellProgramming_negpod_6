21b36fa58e60e067a9eb0aa10d447c31ace4338f
Sng43 <https://github.com/Sng43/MySuperRepo.git>
2024-02-16 07:07:33 -0800

    back-up

--- backup-Negpod_6.sh
+++ backup-Negpod_6.sh
@@ -5,9 +5,9 @@ local_directory="negpod_6-q1"
 
 #remote server details
 
-host="2f05c1f8800b.3be8ebfc.alu-cod.online"
-username="2f05c1f8800b"
-password="d4a1d225d0abda9549d8"
+host="64293e56bc62.3a2627c1.alu-cod.online"
+username="64293e56bc62"
+password="328d3b338a4ced526c9a"
 remote_directory="/summative/1023-2024j"
 
 
