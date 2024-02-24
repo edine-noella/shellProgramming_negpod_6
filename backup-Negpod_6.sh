a7765c3c4106c2be915f1c79879f062d1c1f0f30
Sng43 <https://github.com/Sng43/MySuperRepo.git>
2024-02-24 14:06:15 -0800

    side quest

--- backup-Negpod_6.sh
+++ backup-Negpod_6.sh
@@ -10,7 +10,6 @@ username="64293e56bc62"
 password="328d3b338a4ced526c9a"
 remote_directory="/summative/1023-2024j"
 
-
-rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$local_directory" "$username@$host:$remote_directory"
-
+ 
+rsync -avz -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$local_directory" "$username@$host:$remote_director"
 echo "Backup completed successfully for group $local_directory!"
